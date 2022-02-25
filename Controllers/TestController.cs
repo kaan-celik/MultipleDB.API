d25c68067fb9a060d37742f30eb7ffb82412bbf3
Kaan Celik <kaancelik567@gmail.com>
2022-02-25 22:50:23 +0300

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultipleDB.API.Controllers
+{
+    
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/Test/Test")]
+
+        public IActionResult Test()
+        {
+            return Ok("API Connection Successfull");
+        }
+
+    }
+}
