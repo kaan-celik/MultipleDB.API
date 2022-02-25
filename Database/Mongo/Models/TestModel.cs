dcd4cb9f77647aa35fcc278f269c117f89010b6b
Kaan Celik <kaancelik567@gmail.com>
2022-02-25 22:56:48 +0300

    Test Model added

---
+++ Database/Mongo/Models/TestModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultipleDB.API.Database.Mongo.Models
+{
+    public class TestModel
+    {
+        private int ID { get; set; }
+        private string Value { get; set; }
+
+        public TestModel()
+        {
+
+        }
+
+        public int getID()
+        {
+            return this.ID;
+        }
+
+        public string getValue()
+        {
+            return this.Value;
+        }
+
+        public void setID(int ID)
+        {
+            this.ID = ID;
+        }
+
+        public void setValye(string Value)
+        {
+           this.Value = Value;
+        }
+    }
+}
