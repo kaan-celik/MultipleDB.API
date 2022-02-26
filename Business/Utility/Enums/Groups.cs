954316cdc5b0dca465812386a42e717a2a5f8e1c
Kaan Celik <kaancelik567@gmail.com>
2022-02-26 14:45:40 +0300

    Role and Group enums added

---
+++ Business/Utility/Enums/Groups.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultipleDB.API.Business.Utility.Enums
+{
+    public enum Groups
+    {
+        DEVELOPERS = 1,
+        DESIGNERS = 2,
+        HUMAN_RESOURCES = 3
+    }
+}
---
+++ Business/Utility/Enums/Roles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultipleDB.API.Business.Utility.Enums
+{
+    public enum Roles
+    {
+        MODERATOR = 1,
+        TEAM_LEADER = 2,
+        EMPLOYEE = 3,
+    }
+}
